a48bd126f952c5c859ea55886e8cbe431a8c64bc
barivikranth36 <barivikranth36@gmail.com>
2023-05-10 19:44:08 +0530

    Added preauthorize to every controller

--- src/main/java/com/dd/eraktkendrabackend/Controller/BloodRequestController.java
+++ src/main/java/com/dd/eraktkendrabackend/Controller/BloodRequestController.java
@@ -3,6 +3,7 @@ package com.dd.eraktkendrabackend.Controller;
 import com.dd.eraktkendrabackend.DTO.BloodRequestDTO;
 import com.dd.eraktkendrabackend.Service.BloodRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +17,21 @@ public class BloodRequestController {
     private BloodRequestService bloodRequestService;
 
     // Request for blood by providing userId, BloodType, Quantity, BankId
+    @PreAuthorize("hasRole(ROLE_USER)")
     @PostMapping("/bloodRequestByUser")
     public boolean bloodRequestByUser(@RequestBody BloodRequestDTO bloodRequestDTO) {
         return bloodRequestService.bloodRequestByUser(bloodRequestDTO);
     }
 
     // ---------------------- Check if blood request exist or not on user side ----------------------------
+    @PreAuthorize("hasRole(ROLE_USER)")
     @GetMapping("/checkBloodRequest/{userId}")
     public boolean checkBloodRequest(@PathVariable String userId) {
         return bloodRequestService.checkBloodRequest(Long.parseLong(userId));
     }
 
     // --------------------------------- Revoke the blood request -----------------------------------
+    @PreAuthorize("hasRole(ROLE_USER)")
     @DeleteMapping("/revokeBloodRequest/{userId}")
     public boolean revokeBloodRequest(@PathVariable String userId) {
 //        return true;
@@ -35,18 +39,21 @@ public class BloodRequestController {
     }
 
     // -------------------------- Reject blood request from user by fieldworker -----------------------
+    @PreAuthorize("hasRole(ROLE_FIELDWORKER)")
     @DeleteMapping("/rejectBloodRequest/{userId}")
     public boolean rejectBloodRequest(@PathVariable String userId) {
         return bloodRequestService.revokeBloodRequest(Long.parseLong(userId));
     }
 
     // --------------------------- Field Worker accepts blood request -------------------------------------
+    @PreAuthorize("hasRole(ROLE_FIELDWORKER)")
     @DeleteMapping("/acceptBloodRequest/{bloodRequestId}")
     public boolean acceptBloodRequest(@PathVariable String bloodRequestId) {
         return bloodRequestService.acceptBloodRequest(Long.parseLong(bloodRequestId));
     }
 
     // -------------------------- Get all blood requests ----------------------------------------------
+    @PreAuthorize("hasRole(ROLE_FIELDWORKER)")
     @GetMapping("/getAllBloodRequests/{bankId}")
     public List<BloodRequestDTO> getAllBloodRequests(@PathVariable String bankId) {
         return bloodRequestService.getAllBloodRequests(Long.parseLong(bankId));
